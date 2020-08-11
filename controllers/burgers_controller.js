a8369bd6fa64dfb6fb548b00f56377834062b4d3
Doug Livingston <dliving1.dl@gmail.com>
2020-08-11 15:02:08 -0700

    Update DB, connection.js, server.js

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,9 @@
+const express = require("exprss");
+
+const router = express.Router();
+
+let burger = require("../models/burger.js");
+
+router.get("/", function(req, res) {
+
+})
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (burger_name) VALUES ('Krabby Patty');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Fish Sandwich');
+INSERT INTO burgers (burger_name) VALUES ('Double Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
